0b7d3bb47d3c3497635aa3e32ae190c0cd1d9cf4
Vanja Štefanec <vanja.stefanec@gmail.com>
2023-03-30 12:48:41 +0200

    Compliance with Django>=2.0

--- mockdjangosaml2/urls.py
+++ mockdjangosaml2/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from mockdjangosaml2.views import login, assertion_consumer_service, logout
 
 
 urlpatterns = [
-    url(r'^login/$', login, name='saml2_login'),
-    url(r'^acs/$', assertion_consumer_service, name='saml2_acs'),
-    url(r'^logout/$', logout, name='saml2_logout'),
+    path('login/', login, name='saml2_login'),
+    path('acs/', assertion_consumer_service, name='saml2_acs'),
+    path('logout/', logout, name='saml2_logout'),
 ]
